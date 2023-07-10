37d8b6e7f1b1819b5a7ba8459998230b84c27619
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-07-10 18:28:43 +0300

    Initial Commit

---
+++ Smartway_TZ/Smartway_TZApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smartway_TZApp.swift
+//  Smartway_TZ
+//
+//  Created by Pavel Krivtsov on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Smartway_TZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
