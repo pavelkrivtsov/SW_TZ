9f1934b2ea48a97a483a3a228a6181dd50ca376b
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-07-12 21:13:14 +0300

    Add image scaling

--- Smartway_TZ/View/ImageDetailView.swift
+++ Smartway_TZ/View/ImageDetailView.swift
@@ -11,18 +11,56 @@ import Kingfisher
 struct ImageDetailView: View {
     
     var urlString: String
+    @State private var scale: CGFloat = 1.0
+    @State private var lastScale: CGFloat = 1.0
+    private let minScale: CGFloat = 1.0
+    private let maxScale: CGFloat = 5.0
+    
+    var magnification: some Gesture {
+        MagnificationGesture()
+            .onChanged { state in
+                adjustScale(from: state)
+            }
+            .onEnded { state in
+                withAnimation {
+                    validateScaleLimits()
+                }
+                lastScale = 1.0
+            }
+    }
     
     init(urlString: String) {
         self.urlString = urlString
     }
     
     var body: some View {
-        
         KFImage.url(URL(string: urlString))
             .loadDiskFileSynchronously()
             .cacheMemoryOnly()
             .fade(duration: 0.25)
             .resizable()
             .scaledToFit()
+            .scaleEffect(scale)
+            .gesture(magnification)
+    }
+    
+    private func adjustScale (from state: MagnificationGesture.Value) {
+        let delta = state / lastScale
+        scale *= delta
+        lastScale = state
+    }
+    
+    private func getMinimumScaleAllowed() -> CGFloat {
+        max(scale, minScale)
+    }
+    
+    private func getMaximumScaleAllowed() -> CGFloat {
+        min(scale, maxScale)
+    }
+    
+    private func validateScaleLimits() {
+        scale = getMinimumScaleAllowed()
+        scale = getMaximumScaleAllowed()
     }
 }
+
